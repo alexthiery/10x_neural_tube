93d203fc9c1b03f423f0fcdade2c994661bd9d01
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2020-04-07 14:38:36 +0100

    change local plot path so that plots are in github repo

--- R/1_seurat_full.R
+++ R/1_seurat_full.R
@@ -9,7 +9,7 @@ if (length(args)==0) {
   
   sapply(list.files('/Users/alex/dev/repos/10x_neural_tube/R/my_functions/', full.names = T), source)
   
-  plot.path = "/Users/alex/dev/output/10x_neural_tube/plots/1_seurat_full/"
+  plot.path = "/Users/alex/dev/repos/10x_neural_tube/R/plots/1_seurat_full/"
   rds.path = "/Users/alex/dev/output/10x_neural_tube/RDS.files/1_seurat_full/"
   dir.create(plot.path, recursive = T)
   dir.create(rds.path, recursive = T)
