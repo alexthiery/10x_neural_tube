64fee59c9a86a8ca22427cb9a0008bf584e5c002
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2020-06-18 13:57:29 +0100

    debug

--- bin/R/test.R
+++ bin/R/test.R
@@ -3,9 +3,10 @@ args <- commandArgs(TRUE)
 
 cat("R script is running")
 
-sapply(list.files(args[1], full.names = T), source)
 
 plot_path <- "plots/"
 dir.create(plot_path, recursive = T)
 processed_data <- "processed_data/"
 dir.create(processed_data, recursive = T)
+
+sapply(list.files(args[1], full.names = T), source)
