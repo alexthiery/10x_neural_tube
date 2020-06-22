757ed6fdcb7dd6b646bdbc8570a2b130393da5cd
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2020-06-22 13:10:07 +0100

    tag dir in nextflow main

--- test_run.sh
+++ test_run.sh
@@ -8,7 +8,7 @@ ml Nextflow/20.01.0
 ml Singularity/3.4.2
 ml Graphviz
 
-nextflow run alexthiery/10x_neural_tube \
+nextflow run alexthiery/10x_neural_tube -r nextflow \
 -hub github \
 -profile crick \
 --gtf /camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.97.gtf \
