6cdfeec27d9eb282f8182c97eb6ec6692715eb98
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2020-07-08 14:03:21 +0100

    test custom config

---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=10x
+#SBATCH -t 72:00:00
+#SBATCH --mail-type=ALL,ARRAY_TASKS
+#SBATCH --mail-user=alex.thiery@crick.ac.uk
+
+export TERM=xterm
+
+## LOAD REQUIRED MODULES
+ml purge
+ml Nextflow/20.01.0
+ml Singularity/3.4.2
+ml Graphviz
+
+nextflow pull alexthiery/10x_neural_tube -r dev -hub github
+
+nextflow run alexthiery/10x_neural_tube -r dev \
+-hub github \
+-profile local \
+--metadata /camp/home/thierya/scratch/10x_neural_tube/sampleInfo.csv \
+--gtf /camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.97.gtf \
+--fa /camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.dna.toplevel.fa \
+-resume \
+
+
+nextflow run alexthiery/10x_neural_tube -r dev \
+-hub github \
+--metadata /camp/home/thierya/scratch/10x_neural_tube/sampleInfo.csv \
+--gtf ~/dev/genomes/galgal6/Gallus_gallus.GRCg6a.97.gtf \
+--fa ~/dev/genomes/galgal6/Gallus_gallus.GRCg6a.dna.toplevel.fa \
+-resume \
