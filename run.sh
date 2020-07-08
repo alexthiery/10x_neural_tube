f65b306b3361ed5b48537fda29dde2beed29f0d8
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2020-07-08 15:10:01 +0100

    test run script with changing default resources

--- run.sh
+++ run.sh
@@ -14,18 +14,20 @@ ml Graphviz
 
 nextflow pull alexthiery/10x_neural_tube -r dev -hub github
 
-nextflow run alexthiery/10x_neural_tube -r dev \
+nextflow run alexthiery/10x_neural_tube -r Dev \
 -hub github \
--profile local \
+-profile singularity \
 --metadata /camp/home/thierya/scratch/10x_neural_tube/sampleInfo.csv \
 --gtf /camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.97.gtf \
 --fa /camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.dna.toplevel.fa \
+--memory 100 \
+--max_cpus 16 \
 -resume \
 
 
-nextflow run alexthiery/10x_neural_tube -r dev \
--hub github \
---metadata /camp/home/thierya/scratch/10x_neural_tube/sampleInfo.csv \
---gtf ~/dev/genomes/galgal6/Gallus_gallus.GRCg6a.97.gtf \
---fa ~/dev/genomes/galgal6/Gallus_gallus.GRCg6a.dna.toplevel.fa \
--resume \
+# NXF_VER=20.01.0 nextflow run alexthiery/10x_neural_tube -r Dev \
+# -hub github \
+# -profile custom \
+# --metadata /camp/home/thierya/scratch/10x_neural_tube/sampleInfo.csv \
+# --gtf ~/dev/genomes/galgal6/Gallus_gallus.GRCg6a.97.gtf \
+# --fa ~/dev/genomes/galgal6/Gallus_gallus.GRCg6a.dna.toplevel.fa 
