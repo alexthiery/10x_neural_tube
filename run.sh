285b5fd36b88e0b7286b3889f30ca9987af061c9
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2020-07-09 10:35:39 +0100

    remove module load from crick config and update run.sh

--- run.sh
+++ run.sh
@@ -14,17 +14,10 @@ ml Graphviz
 
 nextflow pull alexthiery/10x_neural_tube -r Dev -hub github
 
-nextflow run alexthiery/10x_neural_tube -r Dev \
+NXF_VER=20.01.0 nextflow run alexthiery/10x_neural_tube -r Dev \
 -hub github \
--c /camp/home/thierya/scratch/10x_neural_tube/crickconf.nf \
+-profile crick \
 --metadata /camp/home/thierya/scratch/10x_neural_tube/sampleInfo.csv \
 --gtf /camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.97.gtf \
---fa /camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.dna.toplevel.fa
-
-
-# NXF_VER=20.01.0 nextflow run alexthiery/10x_neural_tube -r Dev \
-# -hub github \
-# -profile custom \
-# --metadata /camp/home/thierya/scratch/10x_neural_tube/sampleInfo.csv \
-# --gtf ~/dev/genomes/galgal6/Gallus_gallus.GRCg6a.97.gtf \
-# --fa ~/dev/genomes/galgal6/Gallus_gallus.GRCg6a.dna.toplevel.fa 
+--fa /camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.dna.toplevel.fa \
+-resume
